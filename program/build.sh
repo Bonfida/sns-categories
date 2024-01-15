efc72f574e5a397b36dcff51c352507d8ddf675a
DR497 <47689875+dr497@users.noreply.github.com>
2024-01-15 16:10:10 +0800

    chore: solana 1.16

--- program/build.sh
+++ program/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-docker build -t solana_build .
-docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent solana_build:latest cargo build-sbf
+# Check if the Docker image exists
+set -e
+docker build -t sns_categories .
+
+
+solana program dump namesLPneVptA9Z5rqUDD9tMTWEJwofgaYwp8cawRkX target/deploy/spl_name_service.so
+
+
+if [[ ${1} == "build-only" ]]; then
+    echo "Only building..."
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sns_categories:latest /bin/bash -c "cargo build-sbf"
+elif [[ ${1} == "test" ]]; then
+    echo "Running tests..."
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sns_categories:latest /bin/bash -c "cargo test-sbf --features no-signer"
+else
+    echo "Running tests + building..."
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sns_categories:latest /bin/bash -c "cargo test-sbf --features no-signer && cargo build-sbf"
+fi
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -18,3 +18,13 @@ pub mod cpi;
 pub mod utils;
 
 declare_id_with_central_state!("6c4faDgogWngP5dFsj1AKFRDtn9zbsae3sgZPuaPfgLK");
+
+#[cfg(not(feature = "no-entrypoint"))]
+solana_security_txt::security_txt! {
+    name: env!("CARGO_PKG_NAME"),
+    project_url: "http://bonfida.org",
+    contacts: "email:security@bonfida.com,link:https://twitter.com/bonfida",
+    policy: "https://immunefi.com/bounty/bonfida",
+    preferred_languages: "en",
+    auditors: "Halborn"
+}
